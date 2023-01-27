82adf14d7925ae4771c19005ff1a42f86f185acf
jullyrud <94559083+jullyrud@users.noreply.github.com>
2023-01-27 20:59:43 +0100

    fix build

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,7 @@
 export const App = () => {
   return (
     <div>
-      <Section title="">
-        <FeedbackOptions options={['good', 'neutral', 'bad']} ></FeedbackOptions>
-        <Statistics></Statistics>
-      </Section>
+ 
     </div>
   );
 };
